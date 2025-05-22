d39f28baeccf960c4a8263b8a1956e649c9b63ce
Priyanshu Sharma <priyanshu.sharma@noventiq.com>
2025-05-22 11:04:56 +0530

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,10 @@
+row = int(input(" enter the number of rows: "))
+for i in range(1, row + 1):
+
+    for j in range(row - i):
+        print(" ", end="")
+
+    for k in range(2 * i -1):
+        print("*", end="")
+    print()
+    
