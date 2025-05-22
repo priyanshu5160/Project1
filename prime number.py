f584081bb430f3b4720742012d4a7772e4977990
priyanshu5160 <priyanshu.sharma@noventiq.com>
2025-05-22 10:48:51 +0530

    Add files via upload

---
+++ prime number.py
@@ -0,0 +1,15 @@
+def prime(n):
+    if n % 2 == 0:
+        return false
+    else:
+        return True
+    for i in range(3, int(n**0.5)+1, 2):
+        if n % i == 0:
+            return false
+        else:
+            return true
+num= int(input("enter a number: "))
+if prime(num):
+    print("number is prime")
+else:
+    print("number is not prime")
